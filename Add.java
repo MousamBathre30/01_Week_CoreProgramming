b0011b7d419775f2ef76608bfb0491ee5135e79a
Mousam bathre <bathremousam@gmail.com>
2025-01-07 18:43:57 +0530

    Basic program to add two number

---
+++ Add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Add{
+  public static void main (String args[]){
+     Scanner scanner = new Scanner(System.in);
+     int a = scanner.nextInt();
+     int b = scanner.nextInt();
+     int sum = a+b;
+     System.out.println("Addition of two Number is " + sum );
+}
+}
