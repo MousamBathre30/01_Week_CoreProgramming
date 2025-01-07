f2bd977c3813a3b575579f5fe4231ea7f872044a
Mousam bathre <bathremousam@gmail.com>
2025-01-07 18:50:56 +0530

    convert the temprature celcius to farh..

---
+++ Temprature.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Temprature{
+  public static void main (String args[]){
+     Scanner scanner = new Scanner(System.in);
+     System.out.println("Enter the temprature in Celsius");
+     int temp_Cel = scanner.nextInt();
+     int temp_Fah = scanner.nextInt()
+     System.out.println("The temprature in Fahrenheit " + temp_Fah );
+}
+}
